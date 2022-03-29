24d7da594002a2289d0c0a72f40b81081b4dce39
Teresa Campbell <teresa.j.campbell@gmail.com>
2022-03-29 16:18:40 -0400

    completed user model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      unique: true,
+      required: 'You need to provide a pizza name!',
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+    },
+    
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ],
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
+
+// get total count of comments and replies on retrieval
+UserSchema.virtual('friendCount').get(function() {
+  return this.friend.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
